f363fb9dd4d50b77c4785e875ac9d126901c1b56
rajagopalrj001 <rajagopalrj001@gmail.com>
2025-03-13 12:00:57 +0530

    updated

---
+++ README.md
@@ -0,0 +1,1 @@
+# PythonSpeechRecoServer
---
+++ python/translate_server.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from googletrans import Translator
+import os
+
+app = Flask(__name__)
+# Allow requests from your frontend application
+CORS(app, origins=["http://127.0.0.1:5500", "https://groupmeet-kd1o.onrender.com","http://localhost:3000","https://groupmeet-kd1o.onrender.com/","http://127.0.0.1:5000","http://localhost:5175/"])  # Add your production URL
+
+translator = Translator()
+
+@app.route('/translate', methods=['POST'])
+def translate():
+    try:
+        data = request.get_json()
+        text = data.get('text', '')
+        target_lang = data.get('target_lang', 'en')
+        
+        translation = translator.translate(text, dest=target_lang)
+        
+        return jsonify({
+            'translation': translation.text,
+            'source_lang': translation.src,
+            'target_lang': translation.dest
+        })
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
